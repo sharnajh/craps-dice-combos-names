9a632e67aa4a01df3e77dbf3df702d20530db639
Sharna Hossain <sharnajh@gmail.com>
2020-08-08 02:05:57 -0400

    changes

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='craps-pkg-sharnajh',
-    version='0.1.126',
-    description='Returns 2 dice rolls with associated craps nickname',
+    name='craps-dice-combos-names',
+    version='0.1.127.1',
+    description='A miniscule library for generating and identifying dice combos in craps',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/sharnajh/craps_pkg.git',
@@ -16,12 +16,16 @@ setup(
     packages=['craps_pkg'],
 
     classifiers=[
-        'License :: OSI Approved :: BSD License',
+        "License :: OSI Approved :: MIT License",
         'Operating System :: MacOS',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Topic :: Games/Entertainment',
+        'Topic :: Games/Entertainment :: Turn Based Strategy',
+        'Topic :: Games/Entertainment :: Simulation',
+        'Topic :: Games/Entertainment :: Arcade'
     ],
 )
