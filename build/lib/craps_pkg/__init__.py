4b9302d9478cbf38b937280a8b17d785540eb0db
Sharna Hossain <sharnajh@gmail.com>
2020-08-07 23:20:26 -0400

    yay

---
+++ build/lib/craps_pkg/__init__.py
@@ -0,0 +1,53 @@
+import random
+
+dice_combos = {
+    'Snake Eyes': [1, 1],
+    'Australian yo': [1, 2],
+    'Little Joe from Kokomo': [1, 3],
+    'No field five': [1, 4],
+    'Easy 6': [1, 5],
+    "Six one you're done": [1, 6],
+    'Ace caught a deuce': [2, 1],
+    'Ballerina': [2, 2],
+    'The fever': [2, 3],
+    'Jimmie Hicks': [2, 4],
+    'Bennie Blue': [2, 5],
+    'Easy eight!': [2, 6],
+    'Easy four': [3, 1],
+    'Michael Jordan': [3, 2],
+    'Brooklyn Forest': [3, 3],
+    'Big Red': [3, 4],
+    'Eighter from Decatur': [3, 5],
+    'Nina from Pasadena': [3, 6],
+    'Little Pheobe': [4, 1],
+    'Lumber number': [4, 2],
+    'Skinny McKinney': [4, 3],
+    'Square pair': [4, 4],
+    'Railroad nine': [4, 5],
+    'Big one on the end': [4, 6],
+    'Sixie from Dixie': [5, 1],
+    'Skinny Dugan': [5, 2],
+    'Easy eight!!': [5, 3],
+    'Jesse James': [5, 4],
+    'Puppy paws': [5, 5],
+    'Yo': [5, 6],
+    'The Devil': [6, 1],
+    'Easy eight!!!': [6, 2],
+    'Lou Brown': [6, 3],
+    'Tennessee': [6, 4],
+    'Six five no jive': [6, 5],
+    'Midnight': [6, 6]
+}
+
+
+def craps_roll():
+    dice_pair = []
+    dice_pair.append(random.randint(1, 6))
+    dice_pair.append(random.randint(1, 6))
+    for nickname, combo in dice_combos.items():
+        if dice_pair == combo:
+            combo_name = nickname
+    return {
+        "dice_pair": dice_pair,
+        "nickname": combo_name
+    }
