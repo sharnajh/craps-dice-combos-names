8a516973a891d5b93340b683c039957c4e0e7209
Sharna Hossain <sharnajh@gmail.com>
2020-08-07 23:12:25 -0400

    trying

---
+++ README.md
@@ -0,0 +1,27 @@
+# Craps Nicknames
+
+Craps Nicknames is a tiny Python package that returns a randomized dice roll with it's associated craps nickname. The nicknames are based on the following chart by [Vital Vegas](https://vitalvegas.com/colorful-nicknames-dice-combinations-craps/):
+![Craps Nicknames Charts](https://vitalvegas.com/wp-content/uploads/2015/02/craps_dice_rolls_updated.jpg)
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install craps-pkg.
+
+## Usage
+
+```python
+import craps_pkg
+
+craps_pkg.craps_roll()
+# returns {"dice_pair": [1,2], "nickname": "Australian yo"}
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='craps-pkg-sharnajh',
-    version='0.1.0',    
-    description='Craps dice roll and nickname combos',
+    version='0.1.1',
+    description='Returns 2 dice rolls with associated craps nickname',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/sharnajh/craps_pkg.git',
     author='Sharna Hossain',
     author_email='sharnajh@gmail.com',
@@ -11,8 +16,8 @@ setup(
     packages=['craps_pkg'],
 
     classifiers=[
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: MacOS',        
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
